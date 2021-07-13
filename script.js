76d20d68417ad51d5d430fc8b0141bb1a5adc0e3
Juveria Manzar <juveriamanzar29@gmail.com>
2021-07-13 23:55:19 +0530

    position-grid

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-let rowNumberSection=document.querySelector(".row-number-section");
+let rowNumberSection = document.querySelector(".row-number-section");
 
-for(let i=1;i<=100;i++){
-    let div=document.createElement("div")
+//row col
+for (let i = 1; i <= 100; i++) {
+    let div = document.createElement("div")
     div.classList.add("row-number")
-    div.innerText=i;
+    div.innerText = i;
     rowNumberSection.append(div)
 }
 
-let columnTagSection=document.querySelector(".column-tag-section")
-for(let i=0;i<26;i++){
-    let asciiCode=65+i//65+0=A
-    let reqAlpha=String.fromCharCode(asciiCode);
+let cellSection=document.querySelector(".cell-section")
 
-    let div=document.createElement("div")
+let columnTagSection = document.querySelector(".column-tag-section")
+//col
+for (let i = 0; i < 26; i++) {
+    let asciiCode = 65 + i//65+0=A
+    let reqAlpha = String.fromCharCode(asciiCode);
+
+    let div = document.createElement("div")
     div.classList.add("column-tag")
-    div.innerText=reqAlpha;
+    div.innerText = reqAlpha;
     columnTagSection.append(div)
+}
+
+for(let i=1;i<=100;i++){
+    let rowDiv=document.createElement('div')
+    rowDiv.classList.add("row");
+
+    for(let j=0;j<26;j++){
+        let asciiCode=65+j;
+
+        let reqAlpha=String.fromCharCode(asciiCode)
+        let cellAddr=reqAlpha+i;//for i=1 =>a1...z1
+        let cellDiv=document.createElement("div")
+        cellDiv.classList.add("cell")
+        cellDiv.setAttribute("data-attribute",cellAddr);
+        rowDiv.append(cellDiv)
+    }
+
+    cellSection.append(rowDiv)
 }
