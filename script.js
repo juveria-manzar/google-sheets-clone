b69e98e72358c40f5e69c26ec7f478cc09f8b657
Juveria Manzar <juveriamanzar29@gmail.com>
2021-07-14 03:55:56 +0530

    updated

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (let i = 1; i <= 100; i++) {
     rowNumberSection.append(div)
 }
 
-let cellSection=document.querySelector(".cell-section")
+let cellSection = document.querySelector(".cell-section")
 
 let columnTagSection = document.querySelector(".column-tag-section")
 //col
@@ -22,18 +22,33 @@ for (let i = 0; i < 26; i++) {
     columnTagSection.append(div)
 }
 
-for(let i=1;i<=100;i++){
-    let rowDiv=document.createElement('div')
+for (let i = 1; i <= 100; i++) {
+    let rowDiv = document.createElement('div')
     rowDiv.classList.add("row");
 
-    for(let j=0;j<26;j++){
-        let asciiCode=65+j;
+    for (let j = 0; j < 26; j++) {
+        let asciiCode = 65 + j;
 
-        let reqAlpha=String.fromCharCode(asciiCode)
-        let cellAddr=reqAlpha+i;//for i=1 =>a1...z1
-        let cellDiv=document.createElement("div")
+        let reqAlpha = String.fromCharCode(asciiCode)
+        let cellAddr = reqAlpha + i;//for i=1 =>a1...z1
+        let cellDiv = document.createElement("div")
+        cellDiv.setAttribute("contentEditable", true);
         cellDiv.classList.add("cell")
-        cellDiv.setAttribute("data-attribute",cellAddr);
+        cellDiv.setAttribute("data-address", cellAddr);
+        cellDiv.addEventListener("click", function (e) {
+            if (lastCell) {
+                lastCell.classList.remove("cell-selected");
+            }
+
+            e.currentTarget.classList.add("cell-selected");
+
+            lastCell = e.currentTarget;
+
+            let currCellAddress = e.currentTarget.getAttribute("data-address");
+
+            formulaBarSelectedCellArea.innerText = currCellAddress;
+        });
+
         rowDiv.append(cellDiv)
     }
 
